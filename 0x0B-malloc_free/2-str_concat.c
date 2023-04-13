941c4f005e605f6b7238f5d4d433198515321f99
oladeji john <paracletemall2000@gmail.com>
2023-04-12 21:50:24 -0700

    modified 2-str-concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,14 +24,14 @@ char *str_concat(char *s1, char *s2)
 		
 
 		
-	if (s1 == NULL)
+	if (s1 == NULL){
 		
-		s1 = "";
+		s1 = "";}
 		
-	if (s2 == NULL)
+	if (s2 == NULL){
 		
 		s2 = "";
-		
+	}
 
 		
 		i = ci = 0;
